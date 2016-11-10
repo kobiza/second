6abfa2a2b9f9ec9e0b53b030c2bbce9463c10dc3
kobiz <kobiz@wix.com>
2016-11-10 17:17:51 +0200

    devshraga

---
+++ webpack.dev.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+      'webpack-dev-server/client?http://localhost:8080',
+      './src/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"] },
+            { test: /\.rt/, exclude: /node_modules/, loaders: ["react-templates-loader?targetVersion=0.14.0"] },
+            { test: /\.scss$/, exclude: /node_modules/, loaders: ['style', 'css', 'resolve-url', 'sass'] }
+        ]
+    }
+};
